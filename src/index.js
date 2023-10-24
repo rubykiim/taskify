5c160e09d69b3da227165ba672f65bc9203e450a
rubykiim <sk7383@nyu.edu>
2023-10-24 16:33:48 +0800

    handle app_home_opened event

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { App } = require('@slack/bolt');
 const { ChatOpenAI } = require("langchain/chat_models/openai");
 const { PromptTemplate } = require("langchain/prompts");
+const db = require('./db/db')
+const prompt = require('./prompt')
 const dotenv = require('dotenv')
+const { appHomeOpenedCallback } = require('./events/app-home-opened')
 
 dotenv.config()
 
@@ -24,20 +27,7 @@ app.event('message', async ({ event, say }) => {
     // initialize ChatOpenAI
     const chatModel = initializeChatModel();
 
-    const promptTemplate = PromptTemplate.fromTemplate(
-        "I want you to analyze this conversation, {conversation}, whether it can be classified as a task. \
-        In other words, I want you to analyze whether the conversation contains \
-        task and deadline(due date and or time the task needs to be completed). \
-        For example, 'Please complete this survey by this Friday' contains 'survey' that can be a task \
-        and the person who said it wants people to complete it by this Friday. \
-        If the conversation is a task, please tell me the task and the deadline in this format. \
-        Task: the task you found, Deadline: the deadline you found.\
-        Also, if the task contains deadline, please convert it to an actual date and time.\
-        For example, if the task is due by 6pm this Friday, please find out what date 'this Friday' is and tell me the exact date.\
-        When outputting your answer, you don't need to tell me that the conversation can be classified as task.\
-        Do not say anything else but the task and dealine.\
-        If it is not a task, please say this is not a task."
-    );
+    const promptTemplate = PromptTemplate.fromTemplate(prompt.classifyTask);
 
     const chain = promptTemplate.pipe(chatModel);
 
@@ -49,6 +39,9 @@ app.event('message', async ({ event, say }) => {
         text: result.content || 'Hello world!',
     });
 });
+
+// Listen for users opening your App Home
+app.event('app_home_opened', appHomeOpenedCallback);
 
 (async () => {
     try {
