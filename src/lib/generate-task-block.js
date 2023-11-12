d2071552eb06252fe16a75e5deaa916cf6e5201b
rubykiim <sk7383@nyu.edu>
2023-11-12 21:50:08 +0800

    Update generate-task-block.js

--- src/lib/generate-task-block.js
+++ src/lib/generate-task-block.js
@@ -1,32 +1,39 @@
+const { Margin } = require('../components/margin')
+const { Divider } = require("../components/divider");
+
 const generateTaskBlock = (task, userId) => {
     return {
-        type: "section",
-        text: {
-            type: "mrkdwn",
-            text: `${task.description}\n <@${userId}>\n`,
-        },
-        accessory: {
-            type: "overflow",
-            options: [
-                {
-                    text: {
-                        type: "plain_text",
-                        text: "Edit",
-                        emoji: true,
+        taskSection: {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: `${task.description}\n <@${userId}>\n`,
+            },
+            accessory: {
+                type: "overflow",
+                options: [
+                    {
+                        text: {
+                            type: "plain_text",
+                            text: "Edit",
+                            emoji: true,
+                        },
+                        value: "value-0",
                     },
-                    value: "value-0",
-                },
-                {
-                    text: {
-                        type: "plain_text",
-                        text: "Delete task",
-                        emoji: true,
+                    {
+                        text: {
+                            type: "plain_text",
+                            text: "Delete task",
+                            emoji: true,
+                        },
+                        value: "value-1",
                     },
-                    value: "value-1",
-                }
-            ],
-            action_id: "overflow-action",
+                ],
+                action_id: "overflow-action",
+            },
         },
+        margin: Margin,
+        divider: Divider,
     };
 };
 
