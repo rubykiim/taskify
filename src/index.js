2a25b1dfc022834a2b7816b51bd3e4d1d12951d8
rubykiim <sk7383@nyu.edu>
2023-11-12 18:41:27 +0800

    db operation / display saved results in app home

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const { App } = require('@slack/bolt');
 const { ChatOpenAI } = require("langchain/chat_models/openai");
 const { PromptTemplate } = require("langchain/prompts");
-const db = require('./db/db')
 const prompt = require('./prompt')
 const dotenv = require('dotenv')
-const { appHomeOpenedCallback } = require('./events/app-home-opened')
+const { appHomeOpenedCallback } = require('./events/app-home-opened');
+const { conversationContext } = require('@slack/bolt/dist/conversation-store');
+const db = require('./db/db')
+const mongoose = require('mongoose');
+const User = require('./db/model/user')
+const Task = require('./db/model/task')
+const { getUserInfo } = require('./lib/get-user-info')
+
 
 dotenv.config()
 
 const initializeChatModel = () => {
     const chatModel = new ChatOpenAI({
         openAIApiKey: process.env.OPEN_API_KEY,
-        temperature: 0.8
+        temperature: 0.1
     });
     return chatModel
 }
@@ -21,18 +27,58 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 
-app.event('message', async ({ event, say }) => {
-    const text = event.text;
+app.event('message', async ({ event, client, logger, say }) => {
+    const text = event.text
+    const channel = event.channel
+    const author = event.user
 
+    console.log('event', event)
     // initialize ChatOpenAI
     const chatModel = initializeChatModel();
 
     const promptTemplate = PromptTemplate.fromTemplate(prompt.classifyTask);
 
     const chain = promptTemplate.pipe(chatModel);
+    // date, day of the week // Wednesday, Nov 8
 
     const result = await chain.invoke({ conversation: text });
-    console.log(result.content);
+    console.log('LLM result: ', result.content); // string
+
+    const userId = event.user // this is user not the current ME
+    // const userData = await await client.users.info()
+    // console.log('userData', userData)
+
+    // TODO: algo to determine what to save or not
+
+    try {
+        let user = await User.findOne({ userId });
+        if (!user) {
+            user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                userId: userId
+            });
+            await user.save();
+            console.log('User saved!')
+        }
+        console.log('User found!')
+
+        // create new task
+        let task = new Task({
+            // author: userId,
+            channel: channel,
+            description: result.content,
+        })
+        task.assignedUsers.push(user._id);
+        await task.save()
+
+        user.tasks.push(task._id);
+        await user.save();
+        console.log('User updated with new task.');
+
+
+    } catch (error) {
+        logger.error(error)
+    }
 
     // Slack bot action
     say({
